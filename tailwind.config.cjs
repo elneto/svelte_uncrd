55c27124747de94b83daa5baed975156ebc4d1be
elneto <ernesto.araiza@gmail.com>
2022-03-18 11:42:21 -0400

    extending colors instead of substituting them

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,14 +6,18 @@ module.exports = {
     ],
     // more options here
     theme: {
-      colors: {
+      extend:{
+        colors: {
         'un-light-gray': '#f2f2f2',
         'un-dark-gray': '#4d4d4d',
+        'un-menu-dark': '#333333',
         'un-blue': '#009edb',
+        'un-dark-gray': '#4d4d4d',
       },
-      fontFamily:{
-        titles: ['"Roboto Condensed"', '"Helvetica Neue"', 'Arial', 'ui-sans-serif'],
-        body: ["Roboto", "Helvetica Neue", 'Helvetica', 'Arial', 'ui-sans-serif'],
+        fontFamily:{
+          titles: ['"Roboto Condensed"', '"Helvetica Neue"', 'Arial', 'ui-sans-serif'],
+          body: ["Roboto", "Helvetica Neue", 'Helvetica', 'Arial', 'ui-sans-serif'],
+        },
       },
     },
   };
