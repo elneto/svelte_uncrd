2fe8a0047399afef065bc394f3d44942a3ddfbca
elneto <ernesto.araiza@gmail.com>
2022-03-19 12:00:16 -0400

    created constants svelte store to save translations site title is now translated

--- src/components/LogoNameRow.svelte
+++ src/components/LogoNameRow.svelte
@@ -1,14 +1,15 @@
----
+<script>
 // Component Script (JavaScript)
 // Logo and name, you can put search bar here too
 
+export let lang;
 import svgLogo from '../images/logo-en.svg';
----
+import {nameSite} from '../constants.js';
+</script>
 
 <div class="py-10 flex flex-col md:flex-row">
     <img src={svgLogo} alt="UN logo" class="w-72 pr-5 md:border-black md:border-r">
     <div class="mt-2 pl-5 text-un-dark-gray font-bold font-titles text-xl md:text-3xl">
-        United Nations<br>
-        Center for Regional Development
+        {@html nameSite[lang]}
     </div>
 </div>
---
+++ src/constants.js
@@ -0,0 +1,3 @@
+import { readable } from 'svelte/store';
+
+export const nameSite = {'en':'United Nations<br>Center for Regional Development', 'jp':'国連<br>地域開発センター'};
