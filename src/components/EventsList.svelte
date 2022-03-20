5ebaa796f6eb29fec0a76db734f1d38b17707c44
elneto <ernesto.araiza@gmail.com>
2022-03-20 12:09:29 -0400

    events list works but it's mixed

---
+++ src/components/EventsList.svelte
@@ -0,0 +1,11 @@
+<script>
+export let lang;
+export let data;
+</script>
+
+<ul class="prose prose-blue">
+    {#each data as event}
+        <li><a href="/{event.lang}/events/{event.slug}">{event.title}</a></li>
+    {/each}
+</ul>
+
