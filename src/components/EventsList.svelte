9f75d5ba8207e29c727fa03ef9eb7725eacd92e4
elneto <ernesto.araiza@gmail.com>
2022-03-21 07:17:04 -0400

    date is ok for english

--- src/components/EventsList.svelte
+++ src/components/EventsList.svelte
@@ -2,18 +2,30 @@
 export let lang;
 export let data;
 
-// data.map(d => console.log(d.slug));
 // this filters per language and per undefined
 const goodData = data.filter(d => (typeof d.slug !== "undefined" && d.lang.localeCompare(lang) == 0));
-// goodData.map(d => console.log(d.slug));
-
 // const badData = data.filter(d => !goodData.includes(d))
 // badData.map(d => console.log(d));
 </script>
 
-<ul class="prose prose-blue w-max">
-    {#each goodData as event}
-        <li>{event.date} - <a href="/{event.lang}/events/{event.slug}">{event.title}</a></li>
-    {/each}
-</ul>
+<article class="prose min-w-[74%]">
+<h1>Events</h1>
+
+{#each goodData as event}
+    <div class="flex border-l-un-blue border-l-[3px] mb-3 px-2 prose lg:min-w-full">
+        <div class="flex flex-col gap-0 px-3">
+            <span class="text-2xl font-semibold">{ event.date.substring(0,2) }</span>
+            <span class="text-xl font-semibold">{ event.date.substring(3,6) }</span>
+        </div>
+        
+        <div class="flex flex-row">
+            <span>
+                <a href="/{event.lang}/events/{event.slug}">{event.title.length > 80 ? event.title.substring(0,80)+"..." : event.title }</a>
+            </span>
+        </div>
+    </div>
+    <hr class="mt-0 mb-3 w-2/3">
+{/each}
+
+</article>
 
